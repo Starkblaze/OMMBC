6cadd693cfe1217197b679c10df9488714f9c68a
Cedrick <Starkblaze@hotmail.com>
2018-03-08 17:43:15 -0800

    Menu sesion

---
+++ app/src/main/java/com/example/stark/ommbc/MenuSesion.java
@@ -0,0 +1,13 @@
+package com.example.stark.ommbc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_sesion);
+    }
+}
