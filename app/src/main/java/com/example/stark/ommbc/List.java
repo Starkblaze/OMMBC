e8fddd977398ef7ea5bfeb63a8e160f38f2170dc
Alessandro <afoglie@cetys.edu.mx>
2018-03-22 18:37:30 -0700

    Mejoramiento Estilo

--- app/src/main/java/com/example/stark/ommbc/List.java
+++ app/src/main/java/com/example/stark/ommbc/List.java
@@ -1,17 +1,14 @@
 package com.example.stark.ommbc;
 
-import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class List extends AppCompatActivity {
@@ -20,12 +17,14 @@ public class List extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Problemas");
+        setSupportActionBar(myToolbar);
         ArrayList<String> list = new ArrayList<String>();
         for(int i = 0; i < 25 ; i++){
             list.add("Problema "+(i+1));
         }
         ListView prob = (ListView)findViewById(R.id.problemas);
-        TextView nprob = (TextView)findViewById(R.id.nprob);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,R.layout.mylist, list);
         prob.setAdapter(adapter);
         prob.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -41,6 +40,5 @@ public class List extends AppCompatActivity {
                 startActivity(pro);
             }
         });
-        nprob.setText(list.size()+" problemas");
     }
 }
--- app/src/main/java/com/example/stark/ommbc/MainActivity.java
+++ app/src/main/java/com/example/stark/ommbc/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.stark.ommbc;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button b = (Button)findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
