7fd92b5e38841eb987c2d1e6796a17625b16bb6c
Hermescm <hermes.castro.martinez@gmail.com>
2018-04-04 12:03:45 -0700

    Registro y Login

--- app/src/main/java/com/example/stark/ommbc/MainActivity.java
+++ app/src/main/java/com/example/stark/ommbc/MainActivity.java
@@ -1,33 +1,46 @@
 package com.example.stark.ommbc;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin, btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = (Button)findViewById(R.id.button);
-        Button c = (Button)findViewById(R.id.test_tab);
-        b.setOnClickListener(new View.OnClickListener() {
+
+        btnLogin = (Button) findViewById(R.id.menu_button_login);
+        btnLogin.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-            Intent i = new Intent(MainActivity.this, List.class);
-            startActivity(i);
+            public void onClick(View v) {
+                Intent MyIntent = new Intent(MainActivity.this, Activity_Tabbed.class);
+                startActivity(MyIntent);
+                finish(); //Block back button
+
             }
         });
-        c.setOnClickListener(new View.OnClickListener() {
+
+        btnRegister = (Button) findViewById(R.id.menu_button_register);
+
+        btnRegister.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Activity_Tabbed.class);
-                startActivity(i);
+            public void onClick(View v) {
+                Intent MyIntent = new Intent(MainActivity.this, RegisterController.class);
+                startActivity(MyIntent);
             }
         });
     }
+
+
 }
+
